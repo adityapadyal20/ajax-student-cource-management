61d32ac2433f624720e0a762c771122c80782dbf
Aditya Padyal <40382535+adityapadyal20@users.noreply.github.com>
2020-05-17 20:33:01 +0530

    Update JDBCConnection.java

--- src/main/java/util/JDBCConnection.java
+++ src/main/java/util/JDBCConnection.java
@@ -8,7 +8,7 @@ public class JDBCConnection {
 		Connection con = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentinfo","root","aditya8308307728");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentinfo","root","root");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
